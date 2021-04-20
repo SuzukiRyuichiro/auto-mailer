8091cc79253a2777f8782a89f1c0756c76c57000
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-20 13:51:54 +0900

    update

---
+++ test-mailer.rb
@@ -0,0 +1,16 @@
+require 'roo'
+
+# require 'sendgrid-ruby'
+# include SendGrid
+
+# from = Email.new(email: 'dragon.aka.scooter@gmail.com')
+# to = Email.new(email: 'b1801432@gl.aiu.ac.jp')
+# subject = 'Sending with SendGrid is Fun'
+# content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+# mail = Mail.new(from, subject, to, content)
+
+# sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+# response = sg.client.mail._('send').post(request_body: mail.to_json)
+# puts response.status_code
+# puts response.body
+# puts response.headers
